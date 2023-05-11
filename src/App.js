efa0c11350d52a4f96c7542414348102bbb831bf
mehmed <mehmed2kw@gmail.com>
2023-05-11 21:15:22 +0300

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import React from 'react';
+import "./assets/styles/style.scss";
+import Header from './components/Header/Header';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/HomePage/Home';
 
 function App() {
   return (
     <div className="wrapper">
+      <Header />
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
     </div>
   );
 }

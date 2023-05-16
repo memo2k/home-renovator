86e8619ff6521c8155a14587a748bf81c9ee88cb
mehmed <mehmed2kw@gmail.com>
2023-05-16 12:13:05 +0300

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import "./assets/styles/style.scss";
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Home from './pages/HomePage/Home';
 import Services from './pages/ServicesPage/Services';
 import Work from './pages/WorkPage/Work';
+import About from './pages/AboutPage/About';
+import Contact from './pages/ContactPage/Contact';
 
 function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
   return (
     <div className="wrapper">
       <Header />
@@ -16,6 +23,8 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='/services' element={<Services />} />
         <Route path='/work' element={<Work />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
       </Routes>
 
       <Footer />
